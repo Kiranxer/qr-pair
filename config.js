b2a5fcd4bb911a4f02aa334f835b645338fde7c1
кιяαи ѕєя <105064751+Kiranxer@users.noreply.github.com>
2024-03-08 17:48:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://Kiran:kiranxer123@@cluster0.fngfqru.mongodb.net/?retryWrites=true&w=majority",
   SESSION_NAME: process.env.SESSION_NAME || "Bot~"
 };
